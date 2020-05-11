58bd615d80d58a69b2092b21224537ed684ba8e1
henry kirya <henry.kirya@gmail.com>
2020-05-11 14:53:41 +0300

    one third way

--- src/App.svelte
+++ src/App.svelte
@@ -10,13 +10,29 @@
  
 <main class="container mx-auto leading-normal px-8 my-12">
   <h1 class="mb-4 text-3xl">meta tutorial</h1>
-  <p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat est ratione id molestias corporis deleniti nisi totam necessitatibus, porro magni, architecto fuga saepe et soluta nam. Veniam numquam vitae consectetur? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium vel numquam odio placeat quasi voluptas officia veniam ipsum iusto unde, quo nesciunt sint porro explicabo officiis sed eius, veritatis corrupti.</p>
+  <p class="mb-8">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat est ratione id molestias corporis deleniti nisi totam necessitatibus, porro magni, architecto fuga saepe et soluta nam. Veniam numquam vitae consectetur? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium vel numquam odio placeat quasi voluptas officia veniam ipsum iusto unde, quo nesciunt sint porro explicabo officiis sed eius, veritatis corrupti.</p>
 
-  <h2 class="border-b border-black">subheading one</h2>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi possimus officiis cumque delectus, maxime blanditiis repellat beatae ut, molestias tenetur nostrum! Esse fugit minus magni quasi enim dicta, aspernatur ut.</p>
 
-  <h2 class="border-b border-black">subheading two</h2>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi possimus officiis cumque delectus, maxime blanditiis repellat beatae ut, molestias tenetur nostrum! Esse fugit minus magni quasi enim dicta, aspernatur ut.</p>
+  <div class="flex flex-wrap -mx-4">
+    <div class="px-4 mb-4 md:mb-0 w-full md:w-1/3">
+    <h2 class="text-xl font-semibold border-b border-black mb-2">
+    subheading one</h2>
+    <a href="https://www.youtube.com/watch?v=iY8LhL6mqG8&list=PLylMDDjFIp1DJvd5rtumxTaoWd6S4rD9N&index=3">
+    youtube link
+  </a>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi possimus officiis cumque delectus, maxime blanditiis repellat beatae ut, molestias tenetur nostrum! Esse fugit minus magni quasi enim dicta, aspernatur ut.</p>    
+  </div>
+    <div class="px-4 mb-4 md:mb-0 w-full md:w-1/3">
+    <h2 class="text-xl font-semibold border-b border-black mb-2">
+    subheading one</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi possimus officiis cumque delectus, maxime blanditiis repellat beatae ut, molestias tenetur nostrum! Esse fugit minus magni quasi enim dicta, aspernatur ut.</p>    
+  </div>
+    <div class="px-4 mb-4 md:mb-0 w-full md:w-1/3">
+    <h2 class="text-xl font-semibold border-b border-black mb-2">
+    subheading one</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi possimus officiis cumque delectus, maxime blanditiis repellat beatae ut, molestias tenetur nostrum! Esse fugit minus magni quasi enim dicta, aspernatur ut.</p>    
+  </div>
+  </div> 
 
 
 </main>
