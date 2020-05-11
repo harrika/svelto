eedda4059bb3c57a7df58fc5d21f01e9a539ad22
henry kirya <henry.kirya@gmail.com>
2020-05-11 13:57:13 +0300

    initialising hte git repo

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Header from "./Header.svelte";
+</script>
+
+<style>
+</style>
+
+<!-- <Header /> -->
+
+ 
+<main class="container mx-auto leading-normal px-8 my-12">
+  <h1 class="mb-4 text-3xl">meta tutorial</h1>
+  <p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat est ratione id molestias corporis deleniti nisi totam necessitatibus, porro magni, architecto fuga saepe et soluta nam. Veniam numquam vitae consectetur? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium vel numquam odio placeat quasi voluptas officia veniam ipsum iusto unde, quo nesciunt sint porro explicabo officiis sed eius, veritatis corrupti.</p>
+
+  <h2 class="border-b border-black">subheading one</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi possimus officiis cumque delectus, maxime blanditiis repellat beatae ut, molestias tenetur nostrum! Esse fugit minus magni quasi enim dicta, aspernatur ut.</p>
+
+  <h2 class="border-b border-black">subheading two</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi possimus officiis cumque delectus, maxime blanditiis repellat beatae ut, molestias tenetur nostrum! Esse fugit minus magni quasi enim dicta, aspernatur ut.</p>
+
+
+</main>
---
+++ src/Header.svelte
@@ -0,0 +1,13 @@
+<style>
+  h1 {
+    width: 6rem;
+  }
+</style>
+
+<header class="header">
+  <h1>
+    <!-- <img src="logo.png" alt="logo" /> -->
+  </h1>
+  <div class="">NINJA Poll</div>
+  <h2>header stuff listing menu here</h2>
+</header>
